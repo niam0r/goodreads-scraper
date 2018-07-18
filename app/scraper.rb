7a15a25f4ab5fb0ac222db57fb6f4916cc706590
Romain niam0r <niamor@mm.st>
2018-07-18 19:58:41 +0100

    basic scraper working

--- app/scraper.rb
+++ app/scraper.rb
@@ -3,10 +3,18 @@ require 'nokogiri'
 
 url = "https://www.goodreads.com/quotes/list/61714489-romain-niam0r?utf8=%E2%9C%93&sort=date_added"
 doc = Nokogiri::HTML(open(url))
-doc.search('.elementList').each do |element|
+
+elementList = doc.search('.elementList')
+puts elementList.length
+
+elementList.each do |element|
   authorOrTitle = element.search('.authorOrTitle').text.strip
   quoteText = element.search('.quoteText').text.strip
-  # img_src =  element.attribute.('src').value
-  puts img_src
+
+  if element.search('a > img').attribute('src').defined?
+    img_src =  element.search('a > img')&.attribute('src').value
+  end
+
+  p img_src
 end
 
