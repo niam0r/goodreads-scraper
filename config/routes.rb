f07e62555805823e856b78bf409aa70e28bdb6da
Romain niam0r <niamor@mm.st>
2018-07-25 17:14:13 +0100

    implemented routes for api and created controller and view folders

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   resources :quotes, only: [:index, :show]
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :quotes, only: [ :index ]
+    end
+  end
 end
