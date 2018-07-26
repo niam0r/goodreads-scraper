9901755a6aa12a06ec37ab45e15209169bdeaf4a
Romain niam0r <niamor@mm.st>
2018-07-26 16:14:42 +0100

    fixed UTF-8 issue

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,9 +3,9 @@ require 'open-uri'
 Quote.destroy_all
 puts "Destroyed all quotes"
 
-# url = "https://www.goodreads.com/quotes/list/61714489-romain-niam0r?utf8=%E2%9C%93&sort=date_added"
-url = "./app/my_quotes.html"
-doc = Nokogiri::HTML(open(url))
+url = "https://www.goodreads.com/quotes/list/61714489-romain-niam0r?utf8=%E2%9C%93&sort=date_added"
+# url = "./app/my_quotes.html"
+doc = Nokogiri::HTML(open(url), nil, Encoding::UTF_8.to_s)
 
 elementList = doc.search('.elementList')
 puts "There's #{elementList.length} quotes"
@@ -15,7 +15,7 @@ elementList.each do |element|
 
   content = quoteText.split("\n")[0][1...-1]
 
-  author = quoteText.split("\n")[2].tr(',', '')
+  author = quoteText.split("\n")[2].tr(',', '').strip
 
   author_img = element.search('a > img').empty? ? '' : element.search('a > img').attribute('src').value
 
@@ -28,3 +28,7 @@ elementList.each do |element|
     book: book
   })
 end
+
+# def clean(text)
+#   text.gsub(/â\u0080/, -)
+# end
