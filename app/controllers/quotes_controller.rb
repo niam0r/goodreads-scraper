9f2d9e390c75bf2dc419983550b13a2353429f66
Romain niam0r <niamor@mm.st>
2018-09-07 16:57:10 +0100

    refactored and set_quote

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,9 +1,20 @@
 class QuotesController < ApplicationController
+  before_action :set_quote, only: [:show, :destroy]
+
   def index
     @quotes = Quote.all
   end
 
   def show
+  end
+
+  def destroy
+    @quote.destroy
+  end
+
+  private
+
+  def set_quote
     @quote = Quote.find(params[:id])
   end
 end
