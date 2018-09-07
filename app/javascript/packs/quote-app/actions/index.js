51b32a5355303f1996bb9134a00e2878138e1f9c
Romain niam0r <niamor@mm.st>
2018-09-07 18:40:28 +0100

    implemented action creator fetchCars()

---
+++ app/javascript/packs/quote-app/actions/index.js
@@ -0,0 +1,15 @@
+export const FETCH_QUOTES = "FETCH_QUOTES";
+export const REMOVE_QUOTE = "REMOVE_QUOTE";
+
+const BASE_URL = "http://localhost:3000/api/v1";
+
+export function fetchCars() {
+  const url = `${BASE_URL}/quotes`;
+  const promise = fetch(url).then(res => res.json());
+
+  return {
+    type: FETCH_QUOTES,
+    payload: promise
+  };
+}
+
