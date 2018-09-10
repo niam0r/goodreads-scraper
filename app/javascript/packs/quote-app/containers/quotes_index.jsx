47b1fade09c32e28c1845d70b1bd05bdf9de1ec0
Romain niam0r <niamor@mm.st>
2018-09-10 17:34:42 +0100

    implemented basic index, quotes displaying correctly

--- app/javascript/packs/quote-app/containers/quotes_index.jsx
+++ app/javascript/packs/quote-app/containers/quotes_index.jsx
@@ -6,15 +6,32 @@ import { connect } from 'react-redux';
 import { fetchQuotes } from '../actions';
 
 class QuotesIndex extends Component {
+  renderQuotes() {
+    return this.props.quotes.map((quote) => {
+      return (
+        <Link to={`/quotes/${quote.id}`} key={quote.id}>
+          <div className="quote-item">
+            <h3>{quote.author} - {quote.book}</h3>
+            <p>{quote.content}</p>
+          </div>
+        </Link>
+      );
+    });
+  }
 
   render() {
     return (
       <div>
-        QuotesIndex
+        {this.renderQuotes()}
       </div>
     );
   }
 }
 
+function mapStateToProps(reduxState) {
+  return {
+    quotes: reduxState.quotes
+  }
+}
 
-export default QuotesIndex;
+export default connect(mapStateToProps, null)(QuotesIndex);
